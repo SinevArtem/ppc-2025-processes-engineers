bd0d8a20c247d68ffb0f9a136ccaf291d183dc46
SinevArtem <aasinev@inbox.com>
2025-11-24 18:33:19 +0300

    fix 7

--- tasks/sinev_a_min_in_vector/mpi/src/ops_mpi.cpp
+++ tasks/sinev_a_min_in_vector/mpi/src/ops_mpi.cpp
@@ -37,54 +37,56 @@ bool SinevAMinInVectorMPI::PreProcessingImpl() {
 }
 
 bool SinevAMinInVectorMPI::RunImpl() {
-  int proc_num = 0;
-  int proc_rank = 0;
-
+  int proc_num = 0, proc_rank = 0;
   MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
   MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
 
-  std::vector<int> &my_vector = GetInput();
+  std::vector<int> local_data;
+  int global_size = 0;
 
-  uint64_t global_size = my_vector.size();
+  if (proc_rank == 0) {
+    global_size = static_cast<int>(GetInput().size());
+  }
 
-  MPI_Bcast(&global_size, 1, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+  // Рассылаем размер всем процессам
+  MPI_Bcast(&global_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
   if (global_size == 0) {
     GetOutput() = INT_MAX;
     return true;
   }
 
-  // Проверка на переполнение
-  if (global_size > INT_MAX) {
-    return false;
-  }
-
-  int min_number = INT_MAX;
-
   int block_size = global_size / proc_num;
   int remainder = global_size % proc_num;
 
-  int start_index = (proc_rank * block_size) + std::min(proc_rank, remainder);
-  int end_index = start_index + block_size + (proc_rank < remainder ? 1 : 0);
+  int local_size = block_size + (proc_rank < remainder ? 1 : 0);
+  local_data.resize(local_size);
+
+  std::vector<int> sendcounts(proc_num);
+  std::vector<int> displacements(proc_num);
 
   if (proc_rank == 0) {
-    for (int i = start_index; i < end_index; i++) {
-      min_number = std::min(my_vector[i], min_number);
-    }
-  } else if (end_index > start_index) {
-    for (int i = start_index; i < end_index; i++) {
-      min_number = std::min(my_vector[i], min_number);
+    for (int i = 0; i < proc_num; i++) {
+      sendcounts[i] = block_size + (i < remainder ? 1 : 0);
+      displacements[i] = (i * block_size) + std::min(i, remainder);
     }
   }
 
-  for (int i = start_index; i < end_index; i++) {
-    min_number = std::min(my_vector[i], min_number);
+  MPI_Bcast(sendcounts.data(), proc_num, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(displacements.data(), proc_num, MPI_INT, 0, MPI_COMM_WORLD);
+
+  MPI_Scatterv(proc_rank == 0 ? GetInput().data() : nullptr, sendcounts.data(), displacements.data(), MPI_INT,
+               local_data.data(), local_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+  int local_min = INT_MAX;
+  for (int value : local_data) {
+    local_min = std::min(local_min, value);
   }
 
-  int general_min = INT_MAX;
-  MPI_Allreduce(&min_number, &general_min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+  int global_min = INT_MAX;
+  MPI_Allreduce(&local_min, &global_min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
 
-  GetOutput() = general_min;
+  GetOutput() = global_min;
   return true;
 }
 
